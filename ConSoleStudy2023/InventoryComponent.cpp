2eb79300a5299caf6ec4b94add9b9458c376f81b
Jung Rob bi <ziqqagwi9423@gmail.com>
2024-02-29 06:56:38 +0900

    FEAT: Inventory basic 추가

--- ConSoleStudy2023/InventoryComponent.cpp
+++ ConSoleStudy2023/InventoryComponent.cpp
@@ -1,19 +1,42 @@
 #include "InventoryComponent.h"
+#include <windows.h>
 
 const int InventoryComponent::MAX_INVENTORY = 10;
 
 void InventoryComponent::Start()
 {
-
+	for (int i{}; i < MAX_INVENTORY; ++i) {
+		inventory[i] = ItemBundle{ -1, -1 };
+	}
 }
 
 void InventoryComponent::Update(double elapsedTime)
 {
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),
+		COORD{ 44, 22 });
 
+	string str;
+	str += " [INVENTORY]  "s + "\n"s;
+	cout << str;
 }
 
 void InventoryComponent::AddItem(E_OBJECT type)
 {
-
+	auto p = find_if(inventory.begin(), inventory.end(),
+		[type](const auto& rhs) {
+			return rhs.second.objectType == type;
+		});
+	if (p != inventory.end()) {
+		if (p->second.num != -1)
+			++p->second.num;
+	}
+	else {
+		for (int i{}; i < MAX_INVENTORY; ++i) {
+			if (inventory[i].num == -1) {
+				inventory[i] = ItemBundle{ (int)type, 1 };
+				break;
+			}
+		}
+	}
 
 }
